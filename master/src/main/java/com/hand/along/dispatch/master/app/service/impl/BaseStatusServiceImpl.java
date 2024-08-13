db6e72ab3944f936ec5d9dec1786678b274bd4fd
dengzhilong <zhilong.deng@hand-china.com>
2024-08-13 14:25:24 +0800

    [UPDATE]自定义告警

--- master/src/main/java/com/hand/along/dispatch/master/app/service/impl/BaseStatusServiceImpl.java
+++ master/src/main/java/com/hand/along/dispatch/master/app/service/impl/BaseStatusServiceImpl.java
@@ -9,6 +9,8 @@ import com.hand.along.dispatch.common.utils.CommonUtil;
 import com.hand.along.dispatch.master.app.service.BaseStatusService;
 import com.hand.along.dispatch.common.domain.WorkflowExecution;
 import com.hand.along.dispatch.common.infra.mapper.WorkflowExecutionMapper;
+import com.hand.along.dispatch.master.domain.Workflow;
+import com.hand.along.dispatch.master.infra.mapper.WorkflowMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +19,16 @@ public class BaseStatusServiceImpl implements BaseStatusService {
     private final WorkflowExecutionMapper workflowExecutionMapper;
     private final ExecutionLogMapper executionLogMapper;
     private final AlertService alertService;
+    private final WorkflowMapper workflowMapper;
 
     public BaseStatusServiceImpl(WorkflowExecutionMapper workflowExecutionMapper,
                                  ExecutionLogMapper executionLogMapper,
-                                 AlertService alertService) {
+                                 AlertService alertService,
+                                 WorkflowMapper workflowMapper) {
         this.workflowExecutionMapper = workflowExecutionMapper;
         this.executionLogMapper = executionLogMapper;
         this.alertService = alertService;
+        this.workflowMapper = workflowMapper;
     }
 
     /**
@@ -35,14 +40,24 @@ public class BaseStatusServiceImpl implements BaseStatusService {
     @Override
     public void updateWorkflowExecutionStatus(WorkflowExecution workflowExecution, String status) {
         workflowExecution.setExecutionStatus(status);
-        if(CommonConstant.ExecutionStatus.isFinished(status)){
+        String log = StringUtils.join(workflowExecution.getLogs(), "\n");
+        if (CommonConstant.ExecutionStatus.isFinished(status)) {
+            Workflow workflow = workflowMapper.selectByPrimaryKey(workflowExecution.getWorkflowId());
             // todo:告警
-            alertService.alert(AlertInfo.builder().build());
+            alertService.alert(AlertInfo.builder()
+                    .alertDate(CommonUtil.now())
+                    .alertObject(CommonConstant.WORKFLOW)
+                    .workflowName(workflow.getWorkflowName())
+                    .alertType(CommonConstant.ExecutionStatus.isSuccess(status) ? CommonConstant.ALERT_SUCCESS : CommonConstant.ALERT_FAIL)
+                    .mailList("")
+                    .subject("DISPATCH ALERT")
+                    .log(log)
+                    .build());
             workflowExecution.setEndDate(CommonUtil.now());
         }
         workflowExecutionMapper.updateByPrimaryKey(workflowExecution);
         ExecutionLog executionLog = workflowExecution.getExecutionLog();
-        executionLog.setExecutionLog(StringUtils.join(workflowExecution.getLogs(), "\n"));
+        executionLog.setExecutionLog(log);
         executionLogMapper.updateByPrimaryKeyWithBLOBs(executionLog);
     }
 }
